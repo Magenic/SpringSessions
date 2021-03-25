2ccf2d2b35e301cf445c0c87d6bede18e90a57ec
Robert Wroblewski <r-wroblewski@bethel.edu>
2021-03-24 19:02:55 -0500

    simple change

--- SpringSessions/src/main/java/com/robertw/EmployeeSkillsTracker/EmployeeSkillsTrackerApplication.java
+++ SpringSessions/src/main/java/com/robertw/EmployeeSkillsTracker/EmployeeSkillsTrackerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeeSkillsTrackerApplication {
+	// comment to invoke change
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeSkillsTrackerApplication.class, args);
 	}
